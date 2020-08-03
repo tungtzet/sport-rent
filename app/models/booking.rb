f3b2af5d2aca43c646ed41a58fba6384db6e892d
tung nguyen <tungnt@arena.edu.vn>
2020-08-03 17:42:57 +0200

    fixing typo

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :item
-  validates :start_date, :finish_date, presence :true
+  validates :start_date, :finish_date, presence: true
 end

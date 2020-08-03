e8fdd89eb1748f83787ed41cb2739b2a5b1c5de6
tung nguyen <tungnt@arena.edu.vn>
2020-08-03 21:11:54 +0200

    add inclusion validations on Item, create controllers, generate devise details views, set cloudinary in producion

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
+  SPORT_TYPES = ["Sky Diving", "Sky Surfing", "Golf", "Biking", "Motor Racing", "Tennis", "Hockey", "Ice Skating", "Skiing"]
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
   validates :name, :category, :description, :location, :price_per_day, presence: true
   validates :price_per_day, numericality: { only_integer: true }
+  validates :category, inclusion: { in: SPORT_TYPES }
 end

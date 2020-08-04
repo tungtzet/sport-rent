62c6006ff00ad1c47c24c44a0c95782ba0bf5492
tung nguyen <tungnt@arena.edu.vn>
2020-08-04 13:00:12 +0200

    item policy created

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  def show
+    @item = Item.find(params[:id])
+    authorize @item
+  end
 end

dd0ffcda7ec754c48841aea1a5dbd1614e8efefc
Andreas Wittenberg <projekt@webdesignagentur.design>
2020-08-04 11:02:43 +0200

    imtems_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-<<<<<<< HEAD
   def index
     @items = Item.all
   end
@@ -17,6 +16,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :category, :description, :location, :price_per_day)
   end
-=======
->>>>>>> e8fdd89eb1748f83787ed41cb2739b2a5b1c5de6
 end

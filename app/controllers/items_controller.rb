3ee9d57bee51320e5067ed9d7177c71199f91b31
Andreas Wittenberg <projekt@webdesignagentur.design>
2020-08-04 11:37:43 +0200

    items controller index action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,20 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+    @items = policy_scope(Item).order(created_at: :desc)
+  end
+
+  def show
+    @items = set_item
+  end
+
+  private
+
+  def set_item
+    Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :category, :description, :location, :price_per_day)
+  end
 end
